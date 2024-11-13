24f7aa0e3ec2d037eae156ebb0061520a698996a
Tuddy09 <115088565+Tuddy09@users.noreply.github.com>
2024-11-13 19:28:55 +0200

    Changed the entry point of the application to be sign in screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <UserProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack initialRouteName="signin">
         <Stack.Screen name="signin" options={{ headerShown: false }} />
         <Stack.Screen name="register" options={{ headerShown: false }} />
         <Stack.Screen name="lobbyDetails/[id]" options={{ headerShown: false }} />
